78309443457ed19ccf2998b0b33d5f27d61bdc40
Kyoungsoo11 <105273785+Kyoungsoo11@users.noreply.github.com>
2022-07-07 18:22:36 +0900

    Update etc.c

--- etc.c
+++ etc.c
@@ -9,3 +9,19 @@ void Color(int back, int text)
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (back << 4) + text);//handle, color value
 }
+// textcolor(0);    Black
+// textcolor(1);    Dark Blue
+// textcolor(2);	Dark Green	   
+// textcolor(3);	Dark Sky Blue
+// textcolor(4);	Dark Red	  
+// textcolor(5);	Dark Purple
+// textcolor(6);	Dark Yellow	  
+// textcolor(7);	Gray
+// textcolor(8);	Dark Gray	 
+// textcolor(9);	Blue
+// textcolor(10);	Green	  
+// textcolor(11);	Sky Blue
+// textcolor(12);	Red	  
+// textcolor(13);	Purple
+// textcolor(14);	Yellow	  
+// textcolor(15);	White
